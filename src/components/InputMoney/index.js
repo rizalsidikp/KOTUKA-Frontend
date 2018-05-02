b81d3834649184e86eef570ffd49437409b5892f
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-02 13:08:47 +0700

    add new props input money

--- src/components/InputMoney/index.js
+++ src/components/InputMoney/index.js
@@ -4,44 +4,51 @@ import Row from '../Row'
 
 import Select from 'react-select-plus'
 
+import { LogoCircle } from './../../images/'
+import { chunkArray } from './../../services/helper'
+
 import './style.scss'
 
 
 class InputMoney extends Component {
 	constructor(props) {
 		super(props)
-		this.state={
-			selectedOption: '',
-		}
 	}
-	handleChange = (selectedOption) => {
-		this.setState({ selectedOption })
+
+	renderItem = (props) => {
+		return(
+			<div className="Select-value">
+				<img className="select-logo" src={ LogoCircle } />
+				<span className="Select-value-label">{props.value}</span>
+			</div>
+		)
 	}
+	
+
 	render() {
-		const { selectedOption } = this.state
-		const valueSelected = selectedOption && selectedOption.value
-
-		const { disabled = false, value = 0, label = '', theme = 'white', onChange= () => {} } = this.props
-
+		const { disabled = false, value = 0, label = '', theme = 'white', onChange= () => {}, onKeyPress = () => {}, onSelectChange = () => {}, selected = 'GBP' } = this.props
+		const options = chunkArray([
+			{ value: 'GBP', label: 'GBP', img: LogoCircle },
+			{ value: 'IDR', label: 'IDR', img: LogoCircle },
+		], 'label', 'value', 'img')
 		return (
 			<div>
-				<label className={ 'font20 font-weight-bold '.concat(theme === 'blue' ? 'text-blue' : 'text-white') }>{ label }</label>
+				<label className={ 'font20 font-weight-bold '.concat(theme === 'secondary' ? 'text-secondary' : 'text-white') }>{ label }</label>
 				<Row className="im-row">
 					<div className="col col-md-auto im-col no-padding">
 						<Select
 							className="im-select"
 							name="form-field-name"
-							value={ valueSelected }
-							onChange={ this.handleChange }
+							value={ selected }
+							clearable={ false }
+							onChange={ onSelectChange }
 							autosize={ false }
-							options={ [
-								{ value: 'one', label: 'One' },
-								{ value: 'two', label: 'Two' },
-							] }
+							options={ options }
+							valueRenderer={ (props) => this.renderItem(props) }
 						/>
 					</div>
-					<div className={ 'col d-flex '.concat(theme === 'blue' ? 'background-blue-light' : '') }>
-						<input className={ 'im-money '.concat(theme === 'blue' ? 'background-blue-light' : '') } value={ value } disabled={ disabled } onChange={ onChange } />
+					<div className={ 'col d-flex '.concat(theme === 'secondary' ? 'background-secondary-light' : '') }>
+						<input className={ 'im-money '.concat(theme === 'secondary' ? 'background-secondary-light' : '') } value={ value } disabled={ disabled } onChange={ onChange } onKeyPress={ onKeyPress } />
 					</div>
 				</Row>
 			</div>	
@@ -54,7 +61,10 @@ InputMoney.propTypes = {
 	value: PropTypes.string,
 	disabled: PropTypes.bool,
 	theme: PropTypes.string,
-	onChange: PropTypes.func
+	onChange: PropTypes.func,
+	onKeyPress: PropTypes.func,
+	onSelectChange: PropTypes.func,
+	selected: PropTypes.string
 }
 
 export default InputMoney
