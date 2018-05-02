13f0f176b26670159bb984eb412641d63de3faa2
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-02 13:01:42 +0700

    remove unused service

--- src/services/forgotpassword.js
+++
@@ -1,7 +0,0 @@
-import api from './api'
-
-export default {
-  postEmail: email => api.post('/password/forgot', email),
-  postCode: code => api.post('/password/verify_code', code),
-  postNewPassword: newPassword => api.post('/password/reset', newPassword)
-}
