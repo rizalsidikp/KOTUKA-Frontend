ff10be0dfae04f8bdb324ba5b1388768b82f99a1
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-02 13:09:21 +0700

    add props modal login

--- src/components/ModalLogin/index.js
+++ src/components/ModalLogin/index.js
@@ -7,6 +7,7 @@ import './style.scss'
 import strings from '../../localizations'
 import LabelInput from '../LabelInput'
 
+
 class ModalLogin extends Component {
 	constructor(props){
 		super(props)
@@ -15,17 +16,19 @@ class ModalLogin extends Component {
 			password: ''
 		}
 	}
+
 	render() {
+		const { type = 'register', open = false } = this.props
 		return (
-			<Modal open={ this.props.open } onClose={ this.props.onClose } contentStyle='ml-body'>
-				<h2 className="ml-header background-blue font24 text-white font-weight-bold no-margin" >{ strings.lets_start }</h2>
+			<Modal open={ open } onClose={ this.props.onClose } contentStyle='ml-body'>
+				<h2 className="ml-header background-secondary font24 text-white font-weight-bold no-margin" >{ type === 'register' ? strings.lets_start : strings.login }</h2>
 				<div className="ml-content">
-					<LabelInput name='email' label={ strings.your_email } placeholder={ strings.email_address } value={ this.state.email } onChange={ (e) => this.setState({ email: e.target.value }) } />
-					<LabelInput name='password' type='password' label={ strings.password } placeholder={ strings.password } value={ this.state.password } onChange={ (e) => this.setState({ password: e.target.value }) } />
-					<button className="button button-blue-dark full-width button-rounded font-weight-bold login-button button-shadow">{ strings.signup }</button>
-					<label className="font16 text-blue-dark text-center full-width clickable">{ strings.or_signup_with }</label>
-					<button className="button button-red-google full-width button-rounded font-weight-bold modal-button button-shadow">{ strings.google }</button>
-					<button className="button button-blue-facebook full-width button-rounded font-weight-bold modal-button button-shadow">{ strings.facebook }</button>
+					<LabelInput disabled={ this.props.loading } name='email' label={ strings.your_email } placeholder={ strings.email_address } value={ this.state.email } onChange={ (e) => this.setState({ email: e.target.value }) } />
+					<LabelInput disabled={ this.props.loading } name='password' type='password' label={ strings.password } placeholder={ strings.password } value={ this.state.password } onChange={ (e) => this.setState({ password: e.target.value }) } />
+					<button disabled={ this.props.loading } className="button button-primary full-width login-button" onClick={ () => this.props.onLogin(this.state.email, this.state.password) }>{ type === 'register' ? strings.register : strings.login }</button>
+					<label className="font16 text-primary text-center full-width clickable">{ type === 'register' ? strings.or_register_with : strings.or_login_with }</label>
+					<button disabled={ this.props.loading } className="button button-red-google full-width modal-button" onClick={ this.props.onGoogleLogin }>{ strings.google }</button>
+					<button disabled={ this.props.loading } className="button button-secondary-facebook full-width modal-button" onClick={ this.props.onFacebookLogin }>{ strings.facebook }</button>
 				</div>
 			</Modal>
 		)
@@ -34,7 +37,12 @@ class ModalLogin extends Component {
 
 ModalLogin.propTypes = {
 	open: PropTypes.bool,
-	onClose: PropTypes.func
+	onClose: PropTypes.func,
+	type: PropTypes.string,
+	loading: PropTypes.bool,
+	onFacebookLogin: PropTypes.func,
+	onGoogleLogin: PropTypes.func,
+	onLogin: PropTypes.func
 }
 
 export default ModalLogin
