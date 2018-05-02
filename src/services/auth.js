a730571ff9de2fe6ced3527d5901c25c8e677f7f
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-02 13:02:14 +0700

    add login and register service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,38 +1,51 @@
+import firebase from 'firebase'
+import config from './../config'
 import api from './api'
-import config from '../config'
+
+var firebaseConfig = {
+	apiKey: config.FIREBASE_API_KEY,
+	authDomain: config.FIREBASE_AUTH_DOMAIN,
+	databaseURL: config.FIREBASE_DATABASE_URL,
+	projectId: config.FIREBASE_PROJECT_ID,
+	storageBucket: config.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID
+}
+firebase.initializeApp(firebaseConfig)
+
+const google = new firebase.auth.GoogleAuthProvider()
+const facebook = new firebase.auth.FacebookAuthProvider()
+
+
+google.addScope('email').addScope('profile')
+facebook.addScope('email').addScope('public_profile').addScope('user_birthday')
+
 
 export default {
-  login: (email, password, scope) =>
-    api.post('/oauth/token', {
-      username: email,
-      password,
-      client_id: config.CLIENT_ID,
-      client_secret: config.CLIENT_SECRET,
-      grant_type: config.GRANT_TYPE,
-      scope,
-  }),
-  register: (first_name, last_name,email, password, phone_number) =>
-    api.post('/register', {
-      email, password, first_name, last_name, phone_number,
-    }),
-  getUser: () => api.get('/user'),
-  updateProfile: profileData => api.post('/update_profile', { ...profileData }),
-  refreshToken: () => api.get('/refresh-token'),
-  oauthFacebook: (token,) => api.post('/sso/facebook', { token, provider: 'facebook' }),
-  oauthGoogle: token => api.post('/sso/google', { token, provider: 'google' }),
-  forgotPass: email => api.post('/password/forgot', { email }),
-  verificationCode: (email, token) => api.post('/password/verify_code', { email, token }),
-  resetPass: (email, new_password) => api.post('/password/reset', { email, new_password }),
-  myBalance:() => api.get('/fee/mybalance'),
-  updateProfilePicture: (image) => api.postData('/photo_profile', { image }),
-  uploadIdCard: (image) => api.postData('/user/idcard', { image }),
-  getIdCard: () => api.get('/user/idcard'),
-  getAddress: () => api.get('/address'),
-  postAddress: data => api.post('/address', data),
-  myAccount: () => api.get('/accounts'),
-  deleteAccount: id => api.delete(`/accounts/${id}`),
-  updateAccount: (data) => api.post('/accounts/update', { ...data }),
-  addAccount: (data) => api.post('/accounts', { ...data }),
-  logout: () => api.get('/logout'),
-  feederRegister: (data) => api.postData('/feeder', data)
+	googleLogin: () => {
+		return new Promise((resolve, reject) => {
+			firebase.auth().signInWithPopup(google)
+				.then((user) => {
+				// const apiResponse = await auth.oauthGoogle(result.credential.accessToken)
+					resolve(user)
+				}).catch((error) => {
+					reject(error)
+				})
+		})
+	},
+	
+	facebookLogin : () => {
+		return new Promise((resolve, reject) => {
+			firebase.auth().signInWithPopup(facebook)
+				.then((user) => {
+				// const apiResponse = await auth.oauthGoogle(result.credential.accessToken)
+					resolve(user)
+				}).catch((error) => {
+					reject(error)
+				})
+		})
+	},
+	
+	login: (username, password) => api.post('users/signin', { username, password }),
+	exLogin: (payload) => api.post('users/exaccount', { ...payload }),
+	register: (payload) => api.post('users/signup', { ...payload })
 }
