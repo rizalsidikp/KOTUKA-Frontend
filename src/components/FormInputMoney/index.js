421cf54b2477d83af3e1cae11fa0da7f33b7e373
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-03 09:45:03 +0700

    add get start trade function

--- src/components/FormInputMoney/index.js
+++ src/components/FormInputMoney/index.js
@@ -2,75 +2,36 @@ import React, { Component } from 'react'
 import InputMoney from '../InputMoney'
 import PropTypes from 'prop-types'
 import Row from '../Row'
+import { convertMoneyString } from './../../services/helper'
 
-import fx from 'money'
-
-import config from './../../config'
 
 import './style.scss'
 import strings from '../../localizations'
 import { LogoCircle } from './../../images'
-import api from '../../services/api'
-
-import accounting from 'accounting'
 
 class FormInputMoney extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			needMoney : '',
-			haveTransfer: '',
 			typingTimeout: 0,
-			needChoice: 'GBP',
-			haveChoice: 'IDR',
 		}
 	}
 
-	convertMoney = (val) => {
-		let needMoney = parseFloat(val)
-		api.get('latest.json', { baseURL: config.BASE_URL_RATES, params: { app_id: config.RATES_ID } })
-			.then((resp) => {
-				fx.rates = resp.rates
-				let haveTransfer = fx(parseFloat(needMoney)).from(this.state.needChoice).to(this.state.haveChoice)
-				if(this.state.haveChoice === 'IDR'){
-					haveTransfer = Math.round(haveTransfer)
-					haveTransfer = accounting.formatMoney(haveTransfer,'', 0, ',')
-				}else{
-					// haveTransfer = Math.round(haveTransfer * 100) / 100
-					// haveTransfer = haveTransfer.toString().split('.')
-					// haveTransfer[0] = accounting.formatMoney(haveTransfer[0],'', 0, ',')
-					// haveTransfer = haveTransfer.join('.')
-					haveTransfer = Math.round(haveTransfer * 100) / 100
-					haveTransfer = accounting.formatMoney(haveTransfer,'', 2, ',')
-				}
-				if(this.state.needChoice === 'IDR'){
-					needMoney = Math.round(needMoney)
-					needMoney = accounting.formatMoney(needMoney,'', 0, ',')
-				}else{
-					needMoney = Math.round(needMoney * 100) / 100
-					needMoney = accounting.formatMoney(needMoney,'', 2, ',')
-				}
-				haveTransfer = haveTransfer.toString()
-				needMoney = needMoney.toString()
-				this.setState({
-					needMoney,
-					haveTransfer
-				}) // ~8.0424
-			})
-	}
-
-	onChangeMoney = (needMoney) => {
-		this.setState({ needMoney, haveTransfer: 'Loading...' })
+	onChangeMoney = (amountNeed) => {
+		this.props.setLoading(true)
+		this.props.onChangeNeed(amountNeed)
+		this.props.onChangeHave('Loading...')
 		if (this.state.typingTimeout) {
 			clearTimeout(this.state.typingTimeout)
 		}
-		if(needMoney === ''){
-			this.setState({haveTransfer : ''})
+		if(amountNeed === ''){
+			this.props.onChangeHave('')
+			this.props.setLoading(false)			
 		}else{
-			let money = new String(needMoney)
+			let money = new String(amountNeed)
 			this.setState({ 
 				typingTimeout: setTimeout(() => {
-					this.convertMoney(money.replace(/[^a-zA-Z 0-9.]+/g,''))
+					this.props.convertMoney(convertMoneyString(money), this.props.selectedNeed, this.props.selectedHave)
 				}, 1000)
 			})
 		}
@@ -82,26 +43,18 @@ class FormInputMoney extends Component {
 		}
 	}
 
-	onGetRates = () => {
-		// oxr.set({ app_id: config.RATES_ID })
-		// oxr.latest(function(props) {
-		// 	// You can now use `oxr.rates`, `oxr.base` and `oxr.timestamp`
-		// 	return 'tes'
-		// })
-	}
-
 	onChangeSelected = (val, mode) => {
 		if(mode === 'need'){
-			this.setState({ needChoice: val.value })
+			this.props.onSelectNeed(val.value)
 		}
 		if(mode === 'have'){
-			this.setState({ haveChoice: val.value })
+			this.props.onSelectHave(val.value)
 		}
-		this.onChangeMoney(this.state.needMoney)
+		this.onChangeMoney(this.props.amountNeed)
 	}
 
 	render() {
-		const { theme = 'white' } = this.props
+		const { theme = 'white', buttonDisabled = false } = this.props
 		return (
 			<div>
 				<Row className="fim-row">
@@ -109,8 +62,8 @@ class FormInputMoney extends Component {
 						<InputMoney
 							label={ strings.i_need }
 							theme={ theme }
-							value={ this.state.needMoney }
-							selected={ this.state.needChoice }
+							value={ this.props.amountNeed }
+							selected={ this.props.selectedNeed }
 							onSelectChange={ (val) => this.onChangeSelected(val, 'need') }
 							onKeyPress={ this.validateKey }
 							onChange={ (e) => this.onChangeMoney(e.target.value) }
@@ -123,8 +76,8 @@ class FormInputMoney extends Component {
 						<InputMoney 
 							label={ strings.you_have_to_transfer }
 							theme={ theme }
-							value={ this.state.haveTransfer }
-							selected={ this.state.haveChoice }							
+							value={ this.props.amountHave }
+							selected={ this.props.selectedHave }							
 							onSelectChange={ (val) => this.onChangeSelected(val, 'have') }
 							disabled
 						/>
@@ -132,7 +85,7 @@ class FormInputMoney extends Component {
 				</Row>
 				<Row>
 					<div className="col col-xs-12 text-center">
-						<button className="button button-yellow" onClick={ this.onGetRates }>{ strings.get_start }</button>
+						<button className="button button-yellow" onClick={ this.props.onStartTrading } disabled={ buttonDisabled }>{ strings.get_start }</button>
 					</div>
 				</Row>
 			</div>
@@ -141,7 +94,19 @@ class FormInputMoney extends Component {
 }
 
 FormInputMoney.propTypes = {
-	theme: PropTypes.string
+	theme: PropTypes.string,
+	selectedNeed: PropTypes.string,
+	selectedHave: PropTypes.string,
+	onSelectNeed: PropTypes.func,
+	onSelectHave: PropTypes.func,
+	amountNeed: PropTypes.string,
+	amountHave: PropTypes.string,
+	onChangeNeed: PropTypes.func,
+	onChangeHave: PropTypes.func,
+	convertMoney: PropTypes.func,
+	onStartTrading: PropTypes.func,
+	buttonDisabled: PropTypes.bool,
+	setLoading: PropTypes.func
 }
 
 export default FormInputMoney
