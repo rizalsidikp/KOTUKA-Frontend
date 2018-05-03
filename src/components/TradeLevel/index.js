06bcc01bbc45aed92abbeb6743f7c1198dbe617b
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-03 09:46:28 +0700

    implement user trading response

--- src/components/TradeLevel/index.js
+++ src/components/TradeLevel/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Row from '../Row'
 
 import './style.scss'
@@ -6,15 +7,22 @@ import strings from '../../localizations'
 
 class TradeLevel extends Component {
 	render() {
+		const level = this.props.level <= 0 ? 0 : this.props.level <= 3 ? 1 : this.props.level <= 6 ? 2 : 3
 		return (
-			<Row className={ 'no-margin justify-content-end font12 tl-p '.concat('text-secondary') }>
-				<div className="tl-text">Fast {strings.trader}</div>
-				<div className={ 'tl-block '.concat('background-secondary') } />
-				<div className={ 'tl-block '.concat('background-secondary') } />
-				<div className={ 'tl-block '.concat('background-secondary') } />
+			<Row className={ 'no-margin justify-content-end font12 tl-p '.concat(level === 1 ? 'text-primary-dark' : level === 2 ? 'text-primary' : level === 3 ? 'text-secondary' : 'text-gray-80') }>
+				<div className="tl-text font-weight-bold">{
+					level === 1 ? strings.tolerable : level === 2 ? strings.fair : level === 3 ? strings.fast : ''
+				} { level !== 0 && strings.trader}</div>
+				<div className={ 'tl-block '.concat(level === 1 ? 'background-primary-dark' : level === 2 ? 'background-primary' : level === 3 ? 'background-secondary' : 'background-gray-80') } />
+				<div className={ 'tl-block '.concat(level === 2 ? 'background-primary' : level === 3 ? 'background-secondary' : 'background-gray-80') } />
+				<div className={ 'tl-block '.concat(level === 3 ? 'background-secondary' : 'background-gray-80') } />
 			</Row>
 		)
 	}
+}
+
+TradeLevel.propTypes = {
+	level: PropTypes.number
 }
 
 export default TradeLevel
