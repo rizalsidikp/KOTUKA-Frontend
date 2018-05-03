f7779e4a58e556a5ac8f38dc10e4c7cd9fd8e509
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-03 09:51:34 +0700

    add trading service

---
+++ src/services/trading.js
@@ -0,0 +1,7 @@
+import api from './api'
+import config from './../config'
+
+export default {
+	getClosestTrade : (payload) => api.get('inquiries/closesttrade', { params : { ...payload } }),
+	getRates: () => api.get('latest.json', { baseURL: config.BASE_URL_RATES, params: { app_id: config.RATES_ID } })
+}
