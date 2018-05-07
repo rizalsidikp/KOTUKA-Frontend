ffd91e483784153f767e7f2db674f9ef75dcd5e8
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-07 12:32:48 +0700

    add transaction card

--- src/components/TransactionCard/index.js
+++ src/components/TransactionCard/index.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './style.scss'
 
 import Row from '../Row'
 import strings from '../../localizations'
+import { LogoCircle } from './../../images'
+import InquiryBox from '../InquiryBox'
 
 class TransactionCard extends Component {
 	render() {
 		return (
 			<Row className='tran-c-row'>
-				<div className='col col-md-auto align-self-end'>
-					<span className='font16 text-black-semi font-weight-semi-bold'>Waiting for picker</span>
+				<div className='col col-md-auto'>
+					<div className="d-flex tran-r-row-flag">
+						<img src={ LogoCircle } className="tran-c-img" /><span className="font20 text-secondary font-weight-bold">GBP</span>
+						<i className="right tran-c-arrow" />
+						<img src={ LogoCircle } className="tran-c-img" /><span className="font20 text-secondary font-weight-bold">IDR</span>
+					</div>
+					<div className="font12 text-gray">{ strings.transaction_status }</div>
+					<span className='font16 text-black-semi font-weight-semi-bold'>{ this.props.status }</span>
+					<InquiryBox 
+						you
+						status="waiting"
+					/>
+					<InquiryBox 
+						status="completed"
+						name="Chelsea London"
+					/>
+					<InquiryBox 
+						status="waiting"
+						name="Thompson"
+					/>
 				</div>
 				<div className='col d-flex flex-column align-items-end'>
 					<span className="font12 text-secondary font-weight-bold">{ strings.will_get }</span>
@@ -21,6 +42,10 @@ class TransactionCard extends Component {
 			</Row>
 		)
 	}
+}
+
+TransactionCard.propTypes = {
+	status: PropTypes.string
 }
 
 export default TransactionCard
