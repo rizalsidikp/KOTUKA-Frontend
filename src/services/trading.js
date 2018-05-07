63f6ff7a4b15bf968e2889f799e73b6de979baf1
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-07 12:34:32 +0700

    add post trading

--- src/services/trading.js
+++ src/services/trading.js
@@ -3,5 +3,6 @@ import config from './../config'
 
 export default {
 	getClosestTrade : (payload) => api.get('inquiries/closesttrade', { params : { ...payload } }),
-	getRates: () => api.get('latest.json', { baseURL: config.BASE_URL_RATES, params: { app_id: config.RATES_ID } })
+	getRates: () => api.get('latest.json', { baseURL: config.BASE_URL_RATES, params: { app_id: config.RATES_ID } }),
+	postTrade: (payload) => api.post('inquiries', { ...payload })
 }
