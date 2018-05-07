2bc67de09829a792ae50f4db704118831cc3e7b1
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-07 12:31:39 +0700

    add new props trade box

--- src/components/TradeBox/index.js
+++ src/components/TradeBox/index.js
@@ -1,36 +1,82 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import accounting from 'accounting'
+import getSymbolFromCurrency from 'currency-symbol-map'
+
+
 import Row from '../Row'
 import './style.scss'
 import strings from '../../localizations'
 import TradeCard from '../TradeCard'
 import Pagination from '../Pagination'
+import Loading from '../Loading'
 
 class TradeBox extends Component {
 	render() {
 		const { theme = 'white' } = this.props
-		if(!this.props.closestTrade || this.props.closestTrade.length < 1){
+		if(!this.props.isSearching){
 			return null
+		}
+
+		let you_will_get = 0, you_transfer = 0
+		if(this.props.selectedTrades.length > 0){
+			you_will_get = this.props.selectedTrades[0].have_amount
+			if(this.props.selectedTrades[0].have_currency === 'IDR'){
+				you_will_get = Math.round(you_will_get)
+				you_will_get = accounting.formatMoney(you_will_get,'', 0, ',')
+			}else{
+				you_will_get = Math.round(you_will_get * 100) / 100
+				you_will_get = accounting.formatMoney(you_will_get,'', 2, ',')
+			}
+			you_transfer = this.props.selectedTrades[0].need_amount
+			if(this.props.selectedTrades[0].need_currency === 'IDR'){
+				you_transfer = Math.round(you_transfer)
+				you_transfer = accounting.formatMoney(you_transfer,'', 0, ',')
+			}else{
+				you_transfer = Math.round(you_transfer * 100) / 100
+				you_transfer = accounting.formatMoney(you_transfer,'', 2, ',')
+			}
 		}
 		return (
 			<Row>
 				<div className="col col-xs-12 col-md-12 background-secondary-light tb-col">
-					<h2 className="font20 text-secondary font-weight-bold">{ strings.select_trader }</h2>
+					{
+						!this.props.loading && this.props.closestTrade && this.props.closestTrade.length > 0 ?
+							<h2 className="font20 text-secondary font-weight-bold">{ strings.select_trader }</h2>
+							: null
+					}
 					<Row>
 						{
-							this.props.closestTrade && this.props.closestTrade.map((trade, tradeIndex) => {
-								return(
-									<TradeCard 
-										key={ tradeIndex }
-										data={ trade }
-										onSelectTrade={ (trade) => this.props.onSelectTrade(trade) }
-										disabled={ this.props.selectedTrades && this.props.selectedTrades.length > 0 }
-									/>
-								)
-							})
+							this.props.loading ?
+								<div className="d-flex justify-content-center full-width">
+									<Loading />
+								</div> 
+								:
+								this.props.closestTrade && this.props.closestTrade.length > 0 ?
+									this.props.closestTrade.map((trade, tradeIndex) => {
+										return(
+											<TradeCard 
+												key={ tradeIndex }
+												data={ trade }
+												onSelectTrade={ (trade) => this.props.onSelectTrade(trade) }
+												disabled={ this.props.selectedTrades && this.props.selectedTrades.length > 0 }
+											/>
+										)
+									})
+									:
+									<div className="font20 text-secondary font-weight-semi-bold text-center full-width">{ strings.no_post }</div>
 						}
+	
 					</Row>
-					<Pagination />
+					{
+						!this.props.loading && this.props.closestTrade && this.props.closestTrade.length > 0 ?
+							<Pagination 
+								detailPage={ this.props.detailPage }
+								onStartTrading={ (page) => this.props.onStartTrading(page) }						
+								disabled={ this.props.selectedTrades && this.props.selectedTrades.length > 0 }
+							/>
+							: null	
+					}
 					{
 						this.props.selectedTrades && this.props.selectedTrades.length > 0 &&
 					<div className="background-secondary-semi tb-selected-box">
@@ -56,24 +102,27 @@ class TradeBox extends Component {
 					<Row className="background-primary tb-traded-box">
 						<div className="col col-md-4">
 							<p className="font20 text-secondary-semi no-margin font-weight-bold">{ strings.will_get }</p>
-							<p className="font24 text-white font-weight-bold">£ 1,038.23</p>
+							<p className="font24 text-white font-weight-bold">{ getSymbolFromCurrency(this.props.selectedTrades[0].have_currency) } { you_will_get }</p>
 							{/* kotuka fee */}
 							<p className="font14 text-secondary-semi no-margin font-weight-bold">{ strings.kotuka_fee }</p>
 							<p className="font14 text-secondary font-weight-bold">{ strings.fee_percent }</p>
 						</div>
 						<div className="col col-md-5">
 							<p className="font20 text-secondary-semi no-margin font-weight-bold">{ strings.you_have_to_transfer }</p>
-							<p className="font24 text-white font-weight-bold">Rp 20,000,000</p>
+							<p className="font24 text-white font-weight-bold">{ getSymbolFromCurrency(this.props.selectedTrades[0].need_currency) } { you_transfer }</p>
 						</div>
 						<div className="col col-md-3">
-							<button className="button button-yellow full-width">{strings.trade}</button>
+							<button className="button button-yellow full-width" onClick={ this.props.onTradeClick }>{strings.trade}</button>
 						</div>
 					</Row>
 					}
 				</div>
 				<div className="col col-xs-12 col-md-12 text-center">
-					<h2 className={ 'font20 font-weight-semi-bold '.concat(theme === 'secondary' ? 'text-primary' : 'text-white') }>{ strings.no_post_match }</h2>	
-					<button className="button button-primary" onClick={ () => {} } >{ strings.create_post }</	button>			
+					{
+						this.props.closestTrade && this.props.closestTrade.length > 0 &&
+						<h2 className={ 'font20 font-weight-semi-bold '.concat(theme === 'secondary' ? 'text-primary' : 'text-white') }>{ strings.no_post_match }</h2>	
+					}
+					<button className="button button-primary" onClick={ this.props.createPost } >{ strings.create_post }</	button>			
 				</div>
 			</Row>
 		)
@@ -86,6 +135,12 @@ TradeBox.propTypes = {
 	selectedTrades: PropTypes.any,
 	onSelectTrade: PropTypes.func,
 	onRemoveTrade: PropTypes.func,
+	createPost: PropTypes.func,
+	isSearching: PropTypes.bool,
+	loading: PropTypes.bool,
+	detailPage: PropTypes.object,
+	onStartTrading: PropTypes.func,
+	onTradeClick: PropTypes.func,
 }
 
 export default TradeBox
