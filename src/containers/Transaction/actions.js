09b57e8bd478105433dec3b3d5abf68bd71984c4
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-09 09:43:07 +0700

    move firebase function to action

--- src/containers/Transaction/actions.js
+++ src/containers/Transaction/actions.js
@@ -1,7 +1,8 @@
 import * as constants from './constants'
 import transactionService from './../../services/transaction'
-import firebaseService from './../../services/firebase'
 import { setHtmlStorage } from '../../services/helper'
+import firebase from 'firebase'
+require('firebase/database')
 
 export function setLoading(loading) {
 	return { type: constants.SET_LOADING, payload: { loading } }
@@ -46,9 +47,23 @@ export function getTransactions(id){
 				}
 				if(response.result.transaction && response.result.transaction.length > 0 ){
 					if(response.result.transaction[0].transaction_status === 'ON_PROGRESS'){
-						const liveTransaction = await firebaseService.getLiveTransaction(response.result.transaction[0].id)
-						await dispatch(setLiveTransaction(liveTransaction))
-						dispatch(setIsLiveTransaction(true))
+						firebase
+							.auth()
+							.signInWithCustomToken(localStorage.getItem('firebaseToken'))
+							.then(() => {
+								const resp = firebase.database().ref(`transaction/${ response.result.transaction[0].id }`)
+								resp.on('value', async(val) => {
+									console.log(val.val(), 'update')
+									await dispatch(setLiveTransaction(val.val()))
+									dispatch(setIsLiveTransaction(true))
+								})
+							})
+							.catch(function(error) {
+								// Handle Errors here.
+								// var errorCode = error.code
+								// var errorMessage = error.message
+								console.log(error)
+							})
 					}else{
 						dispatch(setLiveTransaction(null))
 						dispatch(setIsLiveTransaction(false))						
