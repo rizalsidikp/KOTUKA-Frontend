98baa62dab5604e66dd8ef7059552cd5366874a9
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-14 12:26:54 +0700

    change styling radiobutton

--- src/components/RadioButton/index.js
+++ src/components/RadioButton/index.js
@@ -5,9 +5,9 @@ import './style.scss'
 
 class RadioButton extends Component {
 	render() {
-		const { label = '', checked = false, name = '', onChange = () => {} } = this.props
+		const { label = '', checked = false, name = '', onChange = () => {}, onClick = () => {} } = this.props
 		return (
-			<label className="radio-container font16 text-secondary"  >{ label }
+			<label className="radio-container font16 text-secondary" onClick={ onClick }  >{ label }
 				<input type="radio" name={ name } checked={ checked } onChange={ onChange } />
 				<span className="checkmark"></span>
 			</label>
@@ -19,7 +19,8 @@ RadioButton.propTypes = {
 	label : PropTypes.string,
 	name : PropTypes.string,
 	checked: PropTypes.bool,
-	onChange: PropTypes.func
+	onChange: PropTypes.func,
+	onClick: PropTypes.func,
 }
 
 export default RadioButton
