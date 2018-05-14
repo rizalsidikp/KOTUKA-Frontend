f8f325bff3867a189efd248c5a6572ad88b3a52c
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-14 12:25:29 +0700

    add recipient reducer

--- src/store.js
+++ src/store.js
@@ -9,13 +9,15 @@ import HomeReducer from './containers/Home/reducer'
 import PostReducer from './containers/Post/reducer'
 import TradeConfirmationReducer from './containers/TradeConfirmation/reducer'
 import TransactionReducer from './containers/Transaction/reducer'
+import RecipientReducer from './containers/Recipient/reducer'
 
 const rootReducer = {
 	headerReducer: HeaderReducer,
 	homeReducer: HomeReducer,
 	postReducer: PostReducer,
 	tradeConfirmationReducer: TradeConfirmationReducer,
-	transactionReducer: TransactionReducer
+	transactionReducer: TransactionReducer,
+	recipientReducer: RecipientReducer
 }
 
 const Immutable = require('immutable')
