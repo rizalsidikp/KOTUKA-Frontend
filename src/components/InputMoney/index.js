28d241f6fc6c402f80ef494b2fa244ab40881755
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-15 16:31:52 +0700

    add view for post confirmation

--- src/components/InputMoney/index.js
+++ src/components/InputMoney/index.js
@@ -7,27 +7,9 @@ import Select from 'react-select-plus'
 import { chunkArray } from './../../services/helper'
 
 import './style.scss'
-import currenciesService from '../../services/currencies'
 
 
 class InputMoney extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			currencies: []
-		}
-	}
-
-	componentWillMount() {
-		this.getCurrencies()
-	}
-
-	getCurrencies = async() => {
-		const response = await currenciesService.getCurrencies()
-		const currencies = response.result
-		this.setState({ currencies })
-	}
-
 	renderItem = (props) => {
 		return(
 			<div className="Select-value">
@@ -40,10 +22,15 @@ class InputMoney extends Component {
 
 	render() {
 		const { disabled = false, value = 0, label = '', theme = 'white', onChange= () => {}, onKeyPress = () => {}, onSelectChange = () => {}, selected = 'GBP' } = this.props
-		const options = chunkArray(this.state.currencies, 'currency_alias', 'currency_alias', 'country_flag')
+		const options = chunkArray(this.props.currencies, 'currency_alias', 'currency_alias', 'country_flag')
 		return (
-			<div>
-				<label className={ 'font20 font-weight-bold '.concat(theme === 'secondary' ? 'text-secondary' : 'text-white') }>{ label }</label>
+			<div className={ theme === 'confirmation' ? 'conf-im-money' : '' }>
+				<label className={
+					theme === 'confirmation' ? 
+						'font14 font-weight-bold no-margin'	
+						:
+						'font20 font-weight-bold '.concat(theme === 'secondary' ? 'text-secondary' : 'text-white') 
+				}>{ label }</label>
 				<Row className="im-row">
 					<div className="col col-md-auto im-col no-padding">
 						<Select
@@ -74,7 +61,8 @@ InputMoney.propTypes = {
 	onChange: PropTypes.func,
 	onKeyPress: PropTypes.func,
 	onSelectChange: PropTypes.func,
-	selected: PropTypes.string
+	selected: PropTypes.string,
+	currencies: PropTypes.array
 }
 
 export default InputMoney
