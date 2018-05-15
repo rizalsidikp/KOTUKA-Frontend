1202f3ae1b88f574127dc705037ec616d8518e0a
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-15 16:28:44 +0700

    add recipient select component

---
+++ src/components/RecipientSelect/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './style.scss'
+
+class RecipientSelect extends Component {
+	render() {
+		const { title = '', onClick = () => {}, active = false } = this.props
+		return (
+			<div className={ 'col col-md-4 '.concat(active ? '' : 'clickable') } onClick={ onClick }>
+				<div className={ 'text-center font14 font-weight-bold rs-card '.concat(active ? 'rs-card-active' : '') }>
+					<div>
+						<div className="rs-circle" />
+						{ title }
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+RecipientSelect.propTypes = {
+	title: PropTypes.string,
+	onClick: PropTypes.func,
+	active: PropTypes.bool
+}
+
+export default RecipientSelect
