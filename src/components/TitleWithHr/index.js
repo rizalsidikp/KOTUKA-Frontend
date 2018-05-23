bdb53efd30770d5be27ef1eb87a6a64f66398479
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-23 14:32:52 +0700

    add class name

--- src/components/TitleWithHr/index.js
+++ src/components/TitleWithHr/index.js
@@ -6,13 +6,14 @@ import './style.scss'
 class TitleWithHr extends Component {
 	render() {
 		return (
-			<h2 className="font14 font-weight-bold text-black-semi twh-h2">{ this.props.title }</h2>
+			<h2 className={ 'font18 font-weight-bold text-black-semi twh-h2 '.concat(this.props.className) }>{ this.props.title }</h2>
 		)
 	}
 }
 
 TitleWithHr.propTypes = {
-	title: PropTypes.string
+	title: PropTypes.string,
+	className: PropTypes.string,
 }
 
 export default TitleWithHr
