d870303f3e886c0270465ede7d7609e9c9e6b05c
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-28 16:49:59 +0700

    add modal state

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -13,16 +13,35 @@ import TradeLevel from '../../components/TradeLevel'
 import TitleWithHr from './../../components/TitleWithHr'
 import ProfileTable from '../../components/ProfileTable'
 import ModalChangePassword from '../../components/ModalChangePassword'
+import ModalIdCard from '../../components/ModalIdCard'
+import ModalEditProfile from '../../components/ModalEditProfile'
 
 class Profile extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			modalChangePassword: false
+			modalChangePassword: false,
+			modalUploadIdCard: false,
+			modalEditProfile: false,
+			imagePreviewUrl: '',
+			file: null,
 		}
 	}
 	componentWillMount() {
+		this.props.getCountries()
 		this.props.getUser(this.props.user.get('id'))
+	}
+	handleImageChange = (e) => {
+		e.preventDefault()
+		let reader = new FileReader()
+		let file = e.target.files[0]
+		reader.onloadend = () => {
+			this.setState({
+				file: file,
+				imagePreviewUrl: reader.result
+			})
+		}
+		reader.readAsDataURL(file)
 	}
 	render() {
 		const { user } = this.props
@@ -53,6 +72,22 @@ class Profile extends Component {
 				value: this.props.user.get('phone')
 			}
 		]
+
+		const addressInfo = [
+			{
+				key: strings.country,
+				value: this.props.user.get('address').get('origin_country')
+			},
+			{
+				key: strings.postalCode,
+				value: this.props.user.get('address').get('post_code')
+			},
+			{
+				key: strings.address,
+				value: this.props.user.get('address').get('address')
+			}
+		]
+		console.log(this.props.user.get('address'))
 		return (
 			<div className="container dashboard-container">
 				<Row className="justify-content-center">
@@ -68,6 +103,9 @@ class Profile extends Component {
 										level={ response }
 									/>
 								</div>
+							</div>
+							<div className="usr-edit d-flex align-items-center ">
+								<label className="font16 text-secondary clickable font-weight-semi-bold" onClick={ () => this.setState({ modalEditProfile: true }) } >{ strings.edit_profile }</label>
 							</div>
 						</div>
 						<hr className="usr-hr" />
@@ -86,11 +124,11 @@ class Profile extends Component {
 									className="font24"
 									title={ strings.settings }
 								/>
-								<div className="font16 text-primary font-weight-semi-bold">
+								<div className="font16 text-secondary font-weight-semi-bold">
 									<label className="clickable" onClick={ () => this.setState({ modalChangePassword: true }) }>{ strings.change_password }</label>
 								</div>
-								<div className="font16 text-primary font-weight-semi-bold">
-									<label className="clickable">{ strings.upload_id_card }</label>
+								<div className="font16 text-secondary font-weight-semi-bold">
+									<label className="clickable" onClick={ () => this.setState({ modalUploadIdCard: true }) }>{ strings.upload_id_card }</label>
 								</div>
 							</div>
 						</Row>
@@ -111,6 +149,9 @@ class Profile extends Component {
 									className="font24"
 									title={ strings.address }
 								/>
+								<ProfileTable
+									data={ addressInfo }
+								/>
 							</div>
 						</Row>
 					</div>
@@ -119,6 +160,27 @@ class Profile extends Component {
 					open={ this.state.modalChangePassword }
 					onClose={ () => this.setState({ modalChangePassword: false }) }
 				/>
+				<ModalEditProfile
+					open={ this.state.modalEditProfile }
+					countries={ this.props.countries }
+					onClose={ () => this.setState({ modalEditProfile: false }) }
+				/>
+				<ModalIdCard
+					open={ this.state.modalUploadIdCard }
+					onButtonClick = { () => this.upload.click() }
+					imagePreviewUrl={ this.state.imagePreviewUrl }
+					onClose={ () => this.setState({ modalUploadIdCard: false }) }
+					onSendImage={ this.onSendImage }
+					loading={ this.props.loading }
+				/>
+				<input 
+					id="file"
+					type="file"
+					accept="image/*"
+					ref={ (ref) => this.upload = ref }
+					onChange={ (e) => this.handleImageChange(e) }
+					className="d-none"
+				/>
 			</div>
 		)
 	}
@@ -126,17 +188,21 @@ class Profile extends Component {
 
 Profile.propTypes = {
 	loading: PropTypes.bool,
+	countries: PropTypes.any,
 	user: PropTypes.object,
 	getUser: PropTypes.func,
+	getCountries: PropTypes.func,
 }
 
 const mapStateToProps = createStructuredSelector({
 	loading: selectors.getLoading(),
-	user: getUser()
+	countries: selectors.getCountries(),
+	user: getUser(),
 })
 
 const mapDispatchToProps = (dispatch) => ({
-	getUser: (id) => dispatch(actions.getUser(id))
+	getUser: (id) => dispatch(actions.getUser(id)),
+	getCountries: () => dispatch(actions.getCountries())
 })
 
 export default connect (mapStateToProps, mapDispatchToProps)(Profile)
