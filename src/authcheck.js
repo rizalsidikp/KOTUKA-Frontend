7877b228c3f5b5d6f62b5056fe39fce8653219ff
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-28 16:47:35 +0700

    add second registration check

--- src/authcheck.js
+++ src/authcheck.js
@@ -5,24 +5,36 @@ import { statusHtmlStorage } from './services/helper'
 export default (ComposedComponent) => {
 	return class AuthCheck extends Component {
 		authenticate = false
+		secondRegistration = false
 		
 		componentWillMount() {
 			const isActive = statusHtmlStorage('accessToken')
+			const secRegist = localStorage.getItem('secondRegistration')
 			if(isActive) {
 				this.authenticate = true
+			}
+			if(secRegist) {
+				this.secondRegistration = true
 			}
 		}
 
 		componentWillUpdate() {
 			const isActive = statusHtmlStorage('accessToken')
+			const secRegist = localStorage.getItem('secondRegistration')			
 			if(isActive) {
 				this.authenticate = true
+			}
+			if(secRegist) {
+				this.secondRegistration = true
 			}
 		}
 
 		render() {
 			if (!this.authenticate) {
 				return(<Redirect to="/" />)
+			}
+			if (this.secondRegistration) {
+				return(<Redirect to="/secondregistration" />)
 			}
 			return (<ComposedComponent />)
 		}
