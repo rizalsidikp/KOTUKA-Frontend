2f781c2792fe70659be579a340ec38034957aae4
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-28 16:50:17 +0700

    ad countries

--- src/containers/Profile/reducer.js
+++ src/containers/Profile/reducer.js
@@ -2,13 +2,16 @@ import * as constants from './constants'
 import { fromJS } from 'immutable'
 
 const INITIAL_STATE = fromJS({
-	loading: false
+	loading: false,
+	countries: []
 })
 
 export default (state = INITIAL_STATE, action) => {
 	switch(action.type) {
 	case constants.SET_LOADING:
 		return state.set('loading',  action.payload.loading)
+	case constants.SET_COUNTRIES:
+		return state.set('countries',  action.payload.countries)
 	case constants.SET_INITIAL_STATE: 
 		return INITIAL_STATE
 	default: return state
--- src/containers/Profile/selectors.js
+++ src/containers/Profile/selectors.js
@@ -6,3 +6,8 @@ export const getLoading = () => createSelector(
 	selectProfile(),
 	state => state.get('loading')
 )
+
+export const getCountries = () => createSelector(
+	selectProfile(),
+	state => state.get('countries')
+)
