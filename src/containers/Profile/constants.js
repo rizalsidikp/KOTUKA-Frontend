b1e49e7c37b32b9826a4b014417babf478c6562a
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-28 16:49:43 +0700

    add set countriea

--- src/containers/Profile/constants.js
+++ src/containers/Profile/constants.js
@@ -1,2 +1,3 @@
 export const SET_LOADING = 'src/containers/Profile/SET_LOADING'
 export const SET_INITIAL_STATE = 'src/containers/Profile/SET_INITIAL_STATE'
+export const SET_COUNTRIES = 'src/containers/Profile/SET_COUNTRIES'
