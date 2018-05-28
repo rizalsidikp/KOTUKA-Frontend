44cd84d7bca4fe9f8589849d506899b9aae3a4e7
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-05-28 16:49:25 +0700

    add set countries and update profile

--- src/containers/Profile/actions.js
+++ src/containers/Profile/actions.js
@@ -1,6 +1,8 @@
 import * as constants from './constants'
 import userService from './../../services/user'
+import countriesService from './../../services/countries'
 import { setHtmlStorage } from '../../services/helper'
+import { setUser } from '../Header/actions'
 
 export function setLoading(loading) {
 	return { type: constants.SET_LOADING, payload: { loading } }
@@ -10,16 +12,57 @@ export function setInitialState() {
 	return { type: constants.SET_INITIAL_STATE }
 }
 
+export function setCountries(countries) {
+	return { type: constants.SET_COUNTRIES, payload: { countries } }
+}
+
 export function getUser(id) {
 	return async(dispatch) => {
 		dispatch(setLoading(true))
 		try {
 			const response = await userService.getUser(id)
-			setHtmlStorage('accessToken', response.token, 1500)			
+			setHtmlStorage('accessToken', response.token, 1500)
+			dispatch(setUser(response.result))
+			if(response.result.address === null || response.result.phone === null){
+				if(!localStorage.getItem('secondRegistration')){
+					localStorage.setItem('secondRegistration', 'true')
+					location.reload()
+				}
+			}else{
+				if(localStorage.getItem('secondRegistration')){
+					localStorage.removeItem('secondRegistration')
+					location.reload()
+				}
+			}
 			console.log(response)
 		} catch (error) {
 			console.log(error)
 		}
 		dispatch(setLoading(false))
+	}
+}
+
+
+export function getCountries() {
+	return async (dispatch) => {
+		try {
+			const response = await countriesService.getCountries()		
+			dispatch(setCountries(response.data))
+		} catch (error) {
+			console.log(error)
+		}
+	}
+}
+
+export function updateProfile(payload, id) {
+	return async (dispatch) => {
+		try{
+			const response  = await userService.updateUser(payload, id)
+			setHtmlStorage('accessToken', response.token, 1500)
+			dispatch(getUser(id))			
+			console.log(response)
+		}catch (error) {
+			console.log(error)
+		}
 	}
 }
